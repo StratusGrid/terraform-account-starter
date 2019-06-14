dd2b9f17e8c376142f77ea92f1bf3ad0f68461df
Chris Hurst <chris.hurst@stratusgrid.com>
2019-06-14 16:37:33 -0400

    updated version of cloudtrail module

--- s3-bucket-cloudtrail.tf
+++ s3-bucket-cloudtrail.tf
@@ -1,6 +1,6 @@
 module "cloudtrail" {
   source = "StratusGrid/cloudtrail/aws"
-  version = "2.0.2"
+  version = "2.0.3"
   name_prefix = "${var.name_prefix}"
   name_suffix = "${local.name_suffix}"
   log_bucket = "${module.s3_bucket_logging.bucket_id}"
