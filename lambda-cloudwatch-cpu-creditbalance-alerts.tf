bc6f82a83155991ec993d711a2e8848c71f8a5e7
Chris Hurst <chris.hurst@stratusgrid.com>
2019-06-20 12:39:23 -0400

    updated default name of cpu creditbalance module

--- lambda-cloudwatch-cpu-creditbalance-alerts.tf
+++ lambda-cloudwatch-cpu-creditbalance-alerts.tf
@@ -1,9 +1,9 @@
-module "cpu-burst-balance-lambda" {
+module "cpu-credit-balance-lambda" {
   source = "StratusGrid/lambda-event-handler-cpu-creditbalance/aws"
   version = "1.0.3"
   name_prefix = "${var.name_prefix}"
   name_suffix = "${local.name_suffix}"
-  unique_name = "event-handler-cpu-burst-balance"
+  unique_name = "event-handler-cpu-credit-balance"
   sns_alarm_target = "${aws_sns_topic.infrastructure_alerts.arn}"
   input_tags  = "${merge(local.common_tags,
     map(
