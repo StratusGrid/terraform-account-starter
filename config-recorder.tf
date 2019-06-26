14fb157a5b4ff6cdc52d01148e33172233f84987
Chris Hurst <chris.hurst@stratusgrid.com>
2019-06-26 11:41:44 -0400

    updated to newer version of config recorder

--- config-recorder.tf
+++ config-recorder.tf
@@ -1,17 +1,16 @@
 module "aws_config_recorder_us_east_1" {
-  # source = "../StratusGrid/terraform-aws-config-recorder"
   source = "StratusGrid/config-recorder/aws"
-  version = "1.0.0"
+  version = "1.0.1"
   log_bucket_id = "${module.s3_bucket_logging.bucket_id}"
+  include_global_resource_types = true #only include global resource on one region to prevent duplicate recording of events
   providers = {
     aws = "aws.us-east-1"
   }
 }
 
 module "aws_config_recorder_us_east_2" {
-  # source = "../StratusGrid/terraform-aws-config-recorder"
   source = "StratusGrid/config-recorder/aws"
-  version = "1.0.0"
+  version = "1.0.1"
   log_bucket_id = "${module.s3_bucket_logging.bucket_id}"
   providers = {
     aws = "aws.us-east-2"
@@ -19,9 +18,8 @@ module "aws_config_recorder_us_east_2" {
 }
 
 module "aws_config_recorder_us_west_1" {
-  # source = "../StratusGrid/terraform-aws-config-recorder"
   source = "StratusGrid/config-recorder/aws"
-  version = "1.0.0"
+  version = "1.0.1"
   log_bucket_id = "${module.s3_bucket_logging.bucket_id}"
   providers = {
     aws = "aws.us-west-1"
@@ -29,9 +27,8 @@ module "aws_config_recorder_us_west_1" {
 }
 
 module "aws_config_recorder_us_west_2" {
-  # source = "../StratusGrid/terraform-aws-config-recorder"
   source = "StratusGrid/config-recorder/aws"
-  version = "1.0.0"
+  version = "1.0.1"
   log_bucket_id = "${module.s3_bucket_logging.bucket_id}"
   providers = {
     aws = "aws.us-west-2"
