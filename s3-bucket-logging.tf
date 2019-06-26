f1aaa02b872ee3a656fa010eca44cebef60523ec
Chris Hurst <chris.hurst@stratusgrid.com>
2019-06-26 11:29:14 -0400

    logging bucket changes needed to support aws config

--- s3-bucket-logging.tf
+++ s3-bucket-logging.tf
@@ -1,6 +1,6 @@
 module "s3_bucket_logging" {
   source  = "StratusGrid/s3-bucket-logging/aws"
-  version = "1.0.1"
+  version = "1.0.2"
   name_prefix = "${var.name_prefix}"
   name_suffix = "${local.name_suffix}"
   input_tags = "${merge(local.common_tags,
