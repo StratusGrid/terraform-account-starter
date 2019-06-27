11486ef003b52b24a05c7e4a56f885c71f06e090
Chris Hurst <chris.hurst@stratusgrid.com>
2019-06-27 11:08:55 -0400

    added more info on providers to eliminate bug when passing vars in at command line

--- -provider.tf
+++ -provider.tf
@@ -10,19 +10,35 @@ provider "aws" {
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
+  allowed_account_ids = "${var.account_numbers}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  token      = "${var.token}"
 }
 
 provider "aws" {
   alias  = "us-east-2"
   region = "us-east-2"
+  allowed_account_ids = "${var.account_numbers}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  token      = "${var.token}"
 }
 
 provider "aws" {
   alias  = "us-west-1"
   region = "us-west-1"
+  allowed_account_ids = "${var.account_numbers}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  token      = "${var.token}"
 }
 
 provider "aws" {
   alias  = "us-west-2"
   region = "us-west-2"
+  allowed_account_ids = "${var.account_numbers}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  token      = "${var.token}"
 }
