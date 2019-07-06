6d9142cdbaa539edbd3865c3f6716909005695a8
Chris Hurst <chris.hurst@stratusgrid.com>
2019-07-06 14:21:12 -0400

    updated iam trusting module to 1.1.1

--- iam-role-restricted-admin.tf
+++ iam-role-restricted-admin.tf
@@ -1,7 +1,7 @@
 #With the current code, and on version .11.x, you need to apply the policy one time with -target before this, or face a 'count' error
 module "restricted_admin" {
   source = "StratusGrid/iam-role-cross-account-trusting/aws"
-  version = "1.0.8"
+  version = "1.1.1"
   # source = "github.com/StratusGrid/terraform-aws-iam-role-cross-account-trusting"
   role_name = "${var.name_prefix}-restricted-admin-role${local.name_suffix}"
   principal_arns = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
--- iam-role-restricted-read-only.tf
+++ iam-role-restricted-read-only.tf
@@ -1,7 +1,7 @@
 #With the current code, and on version .11.x, you need to apply the policy one time with -target before this, or face a 'count' error
 module "restricted_read_only" {
   source = "StratusGrid/iam-role-cross-account-trusting/aws"
-  version = "1.0.8"
+  version = "1.1.1"
   # source = "github.com/StratusGrid/terraform-aws-iam-role-cross-account-trusting"
   role_name = "${var.name_prefix}-restricted-read-only-role${local.name_suffix}"
   principal_arns = ["arn:aws:iam::${var.users_account_number}:root"]
