2c566d41dd885a5822c77c247dc1cb7cdd600fb3
Tyler Martin <tyler.martin@stratusgrid.com>
2019-07-09 11:15:56 -0500

    Fixes principle arn in read only role

--- iam-role-restricted-read-only.tf
+++ iam-role-restricted-read-only.tf
@@ -4,7 +4,7 @@ module "restricted_read_only" {
   version = "1.1.1"
   # source = "github.com/StratusGrid/terraform-aws-iam-role-cross-account-trusting"
   role_name = "${var.name_prefix}-restricted-read-only-role${local.name_suffix}"
-  principal_arns = ["arn:aws:iam::${var.users_account_number}:root"]
+  principal_arns = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
   policy_arns = ["${aws_iam_policy.read_only_restrictions.arn}","${data.aws_iam_policy.read_only_access.arn}"]
   require_mfa = true
   input_tags  = "${merge(local.common_tags,
