703671e0955beab015c77189995c18d14d0aa142
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-03-24 15:50:52 -0400

    Billing Alerts

---
+++ billing-alerts.tf
@@ -0,0 +1,7 @@
+module "cost-billing-alarm" {
+  source                    = "binbashar/cost-billing-alarm/aws"
+  version                   = "1.0.12"
+  monthly_billing_threshold = var.monthly_billing_threshold
+  currency                  = var.currency
+  aws_env                   = var.env_name
+}
