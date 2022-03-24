5b2b61afdb8e070d6b03129e2b1fea107a3b318f
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-03-24 13:21:30 -0400

    Provider Tagging Update

--- provider.tf
+++ provider.tf
@@ -1,26 +1,54 @@
 provider "aws" {
   region              = var.region
-  allowed_account_ids = var.account_numbers
+  default_tags {
+    tags = merge(
+      local.common_tags,
+      { Region_Purpose = "Active" }
+    )
+  }
 }
 
 #Extra Providers for Config and other Multi-Region configurations like AWS Config
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
+  default_tags {
+    tags = merge(
+      local.common_tags,
+      { Region_Purpose = "Active" }
+    )
+  }
 }
 
 provider "aws" {
   alias  = "us-east-2"
   region = "us-east-2"
+  default_tags {
+    tags = merge(
+      local.common_tags,
+      { Region_Purpose = "Active" }
+    )
+  }
 }
 
 provider "aws" {
   alias  = "us-west-1"
   region = "us-west-1"
+  default_tags {
+    tags = merge(
+      local.common_tags,
+      { Region_Purpose = "Active" }
+    )
+  }
 }
 
 provider "aws" {
   alias  = "us-west-2"
   region = "us-west-2"
+  default_tags {
+    tags = merge(
+      local.common_tags,
+      { Region_Purpose = "Active" }
+    )
+  }
 }
-
