56c3503660a4c8fd732b49fec486d81b4e6913df
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-03-24 16:23:57 -0400

    Update version based upon backend tag failure

--- config-recorder.tf
+++ config-recorder.tf
@@ -2,7 +2,7 @@ module "aws_config_recorder_us_east_1" {
   count = var.control_tower_enabled == false ? 1 : 0
 
   source                        = "StratusGrid/config-recorder/aws"
-  version                       = "1.0.2"
+  version                       = "1.1.0"
   log_bucket_id                 = module.s3_bucket_logging_us_east_1.bucket_id
   include_global_resource_types = true #only include global resource on one region to prevent duplicate recording of events
   providers = {
@@ -14,7 +14,7 @@ module "aws_config_recorder_us_east_2" {
   count = var.control_tower_enabled == false ? 1 : 0
 
   source        = "StratusGrid/config-recorder/aws"
-  version       = "1.0.2"
+  version       = "1.1.0"
   log_bucket_id = module.s3_bucket_logging_us_east_2.bucket_id
   providers = {
     aws = aws.us-east-2
@@ -25,7 +25,7 @@ module "aws_config_recorder_us_west_1" {
   count = var.control_tower_enabled == false ? 1 : 0
 
   source        = "StratusGrid/config-recorder/aws"
-  version       = "1.0.2"
+  version       = "1.1.0"
   log_bucket_id = module.s3_bucket_logging_us_west_1.bucket_id
   providers = {
     aws = aws.us-west-1
@@ -36,7 +36,7 @@ module "aws_config_recorder_us_west_2" {
   count = var.control_tower_enabled == false ? 1 : 0
 
   source        = "StratusGrid/config-recorder/aws"
-  version       = "1.0.2"
+  version       = "1.1.0"
   log_bucket_id = module.s3_bucket_logging_us_west_2.bucket_id
   providers = {
     aws = aws.us-west-2
