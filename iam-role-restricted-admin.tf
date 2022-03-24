6e471183bc0e944fdab0db328c90bea43eb501a7
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-03-24 13:16:42 -0400

    TF FMT

--- iam-role-restricted-admin.tf
+++ iam-role-restricted-admin.tf
@@ -4,7 +4,7 @@ module "restricted_admin" {
   # source = "github.com/StratusGrid/terraform-aws-iam-role-cross-account-trusting"
 
   role_name      = "${var.name_prefix}-restricted-admin-role${local.name_suffix}"
-  principal_arns = distinct(flatten(["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",var.trusted_users_account_arns]))
+  principal_arns = distinct(flatten(["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root", var.trusted_users_account_arns]))
   policy_arns    = [aws_iam_policy.restricted_admin.arn]
   require_mfa    = false
   input_tags     = merge(local.common_tags, {})
--- sns-topics.tf
+++ sns-topics.tf
@@ -1,6 +1,6 @@
 # infrastructure_alerts is used to alert on infrastructure monitoring alarms etc.
 resource "aws_sns_topic" "infrastructure_alerts" {
-  name = "${var.name_prefix}-infrastructure-alerts${local.name_suffix}"
-  tags = merge(local.common_tags, {})
+  name     = "${var.name_prefix}-infrastructure-alerts${local.name_suffix}"
+  tags     = merge(local.common_tags, {})
   provider = aws.us-east-1
 }
