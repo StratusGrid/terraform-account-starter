cb20727338b8e648f26b09347ec66c207fd87868
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-03-25 09:33:32 -0400

    Terrascan config

--- s3-bucket-terraform-state.tf
+++ s3-bucket-terraform-state.tf
@@ -1,7 +1,6 @@
 module "terraform_state" {
   source  = "StratusGrid/terraform-state-s3-bucket-centralized-with-roles/aws"
   version = "2.1.0"
-  # source  = "github.com/StratusGrid/terraform-aws-terraform-state-s3-bucket-centralized-with-roles"
 
   name_prefix   = var.name_prefix
   name_suffix   = local.name_suffix
