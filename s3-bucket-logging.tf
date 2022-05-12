1d2272c588565f0e1722a8a8410e59bbabaed4d2
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-05-12 10:19:56 -0400

    Upgraded Sub Modules

--- s3-bucket-logging.tf
+++ s3-bucket-logging.tf
@@ -1,8 +1,6 @@
 module "s3_bucket_logging_us_east_1" {
-  /* source      = "StratusGrid/s3-bucket-logging/aws" */
-  /* version     = "~> 2.0" */
-
-  source = "github.com/StratusGrid/terraform-aws-s3-bucket-logging.git?ref=aws4.x"
+  source  = "StratusGrid/s3-bucket-logging/aws"
+  version = "~> 2.0"
 
   name_prefix = var.name_prefix
   name_suffix = "${local.name_suffix}-us-east-1"
@@ -19,10 +17,8 @@ module "s3_bucket_logging_us_east_1" {
 }
 
 module "s3_bucket_logging_us_east_2" {
-  /* source      = "StratusGrid/s3-bucket-logging/aws" */
-  /* version     = "~> 2.0" */
-
-  source = "github.com/StratusGrid/terraform-aws-s3-bucket-logging.git?ref=aws4.x"
+  source  = "StratusGrid/s3-bucket-logging/aws"
+  version = "~> 2.0"
 
   name_prefix = var.name_prefix
   name_suffix = "${local.name_suffix}-us-east-2"
@@ -39,10 +35,8 @@ module "s3_bucket_logging_us_east_2" {
 }
 
 module "s3_bucket_logging_us_west_1" {
-  /* source      = "StratusGrid/s3-bucket-logging/aws" */
-  /* version     = "~> 2.0" */
-  
-  source = "github.com/StratusGrid/terraform-aws-s3-bucket-logging.git?ref=aws4.x"
+  source  = "StratusGrid/s3-bucket-logging/aws"
+  version = "~> 2.0"
 
   name_prefix = var.name_prefix
   name_suffix = "${local.name_suffix}-us-west-1"
@@ -59,11 +53,9 @@ module "s3_bucket_logging_us_west_1" {
 }
 
 module "s3_bucket_logging_us_west_2" {
-  /* source      = "StratusGrid/s3-bucket-logging/aws" */
-  /* version     = "~> 2.0" */
+  source  = "StratusGrid/s3-bucket-logging/aws"
+  version = "~> 2.0"
 
-  source = "github.com/StratusGrid/terraform-aws-s3-bucket-logging.git?ref=aws4.x"
-  
   name_prefix = var.name_prefix
   name_suffix = "${local.name_suffix}-us-west-2"
   input_tags  = merge() # This is blank for module compatibility, we feed it null tags as our provider level will take over
