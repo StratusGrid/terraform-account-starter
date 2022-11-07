c286c5b4ae826ad6083415f3e54d786f7b575a63
JoseMotto-sg <jose.motto@stratusgrid.com>
2022-11-07 16:11:40 +0100

    template refactor

--- lambda-cloudwatch-ebs-burstbalance-alerts.tf
+++ lambda-cloudwatch-ebs-burstbalance-alerts.tf
@@ -1,10 +1,11 @@
 module "ebs_burst_balance_lambda" {
   source  = "StratusGrid/lambda-event-handler-ebs-burstbalance/aws"
-  version = "~> 2.0"
+  version = "2.1.0"
 
-  name_prefix      = var.name_prefix
-  name_suffix      = local.name_suffix
-  unique_name      = "event-handler-ebs-burst-balance"
-  sns_alarm_target = aws_sns_topic.infrastructure_alerts.arn
-  input_tags       = merge() # This is blank for module compatability, we feed it null tags as our provider level will take over
+  name_prefix                 = var.name_prefix
+  name_suffix                 = local.name_suffix
+  unique_name                 = "event-handler-ebs-burst-balance"
+  sns_alarm_target            = aws_sns_topic.infrastructure_alerts.arn
+  kms_log_key_deletion_window = 30
+  input_tags                  = merge() # This is blank for module compatability, we feed it null tags as our provider level will take over
 }
