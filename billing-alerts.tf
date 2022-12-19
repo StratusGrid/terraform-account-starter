4b5fde6a833dfe7018b7502d3f2b7d10b19c066d
Christopher Childress <chris.childress@stratusgrid.com>
2022-12-19 14:38:39 -0500

    style: code organization and comment refinement

--- billing-alerts.tf
+++ billing-alerts.tf
@@ -1,8 +1,12 @@
 module "aws_anomaly_detection_alerts" {
-  count      = var.payer_account == true ? 1 : 0 # Only enable if this is a payer account
-  source     = "StratusGrid/anomaly-detection-alerts/aws"
-  version    = "~> 3.0"
-  input_tags = merge() # This is blank for module compatability, we feed it null tags as our provider level will take over
+  count = var.payer_account == true ? 1 : 0 # Only enable if this is a payer account
+
+  source  = "StratusGrid/anomaly-detection-alerts/aws"
+  version = "~> 3.0"
+
+  enable_cost_anomaly_detection = var.payer_account
+
+  input_tags = merge() # Common tags applied at provider level - additional tags may be added here
 
   cost_threshold     = var.cost_anomaly_billing_threshold # This is a sample of $100
   subscription_email = var.cost_anomaly_subscription_email
